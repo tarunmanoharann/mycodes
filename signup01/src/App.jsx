28c7e301f853ead7f89a5d61b810626c31158a5f
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-05-15 22:38:28 +0530

    version 1

--- signup01/src/App.jsx
+++ signup01/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Signup from './components/signup'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Signup/>
     </>
   )
 }
